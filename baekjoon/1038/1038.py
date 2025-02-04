91ac86dde883885325493efba72fbc0abac3e543
김선표 <2020112030@linuxserver1.a.mme.dongguk.edu>
2025-02-04 21:00:31 +0900

    [1038] ~ing

--- baekjoon/1038/1038.py
+++ baekjoon/1038/1038.py
@@ -4,51 +4,63 @@
 
 import numpy as np
 
-matrix = np.zeros((10, 10), dtype=int)
-
 # N 이 존재하는 행렬 만들기
+matrix = np.zeros((10, 10), dtype=int)
+matrix[0] = 1
 for i in range(10) :
-
     for j in range(10):
-        if i == 0:
-            matrix[i,j] = 1
-        
-        if i == j:
-            matrix[i,j] = 1
-        
         if i > 0 and j > 0 and i <= 10 and j <= 10 :
             matrix[i,j] = matrix[i, j-1] + matrix[i-1, j-1]
 
 # Input
 N = int(input())
 
-N_i = 0 
-N_j = 0
-# 이전 행의 합 값
-matrix_total = 0
-temp = 0
 find = 0
 
-for i in range(10) :
-    # 현재 행의 크기 합
-    temp = np.sum(matrix[i]) - 1
-    matrix_total = matrix_total + temp
+def find_X(N, find) :
+    N_i = 0 
+    N_j = 0
+    matrix_total = 0
+    temp = 0
+    find = find
+    out = False
 
-    # N이 해당 행보다 크면 다른 행에 N값 존재
-    if N > matrix_total :
-            pass
-    else : 
-        N_i = i
-        find = matrix_total - temp
-        for j in range(10):
-            find = find + matrix[i, j]
-            if N < find :
-                N_j = j
-                break
-        break
+    for i in range(10) :
+        # 현재 행의 크기 합
+        temp = np.sum(matrix[i]) - 1
+        matrix_total = matrix_total + temp
 
-find = find - N
+        # 못 찾을 경우 없는 수
+        if N > np.sum(matrix):
+            out = True
+
+        # N이 해당 행보다 크면 다른 행에 N값 존재
+        if N > matrix_total :
+                pass
+        else : 
+            N_i = i
+            find = matrix_total - temp
+            for j in range(10):
+                if N <= find + matrix[i, j] :
+                    N_j = j
+                    break
+                find = find + matrix[i, j]
+            break
+
+    return N_i, N_j, find, out
 
 # output
 X = 0
+I, J, find, out = find_X(N, 0)
 
+# 범위 이탈
+if out is True:
+    print(int(-1))
+    exit()
+
+for i in range(I, -1, -1) :
+    X = X + 10**I * J
+    find = N - find
+    I, J, find, out = find_X(find, 0)
+
+print(X)
