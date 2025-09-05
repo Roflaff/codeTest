df38ee4e78af2b7f21098279b719c2a996a38d8e
Roflaff <kimsp0317@gmail.com>
2025-09-05 04:54:28 +0000

    samsung

--- samsung/test1.py
+++ samsung/test1.py
@@ -10,6 +10,11 @@ for test_case in range(1, T + 1):
     # N * N 배열 입력 받기
     arr = [list(map(int, input().split())) for _ in range(N)]
     
+    """
+    - 7 ≤  N ≤ 12
+    - 1 ≤ core ≤ 12
+    
+    """
     
     print(arr)
     print(f"#{test_case} {result}")
